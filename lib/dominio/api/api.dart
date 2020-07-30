2d19624dd376697f9d8bba7b3262264bd963dbaf
Lucas Barros Santos <lucasbarrossantos@MacBook-Pro-de-Lucas.local>
2020-07-30 11:47:25 -0300

    Refatoracao da class API para remover duplicidade de variaveis

--- lib/dominio/api/api.dart
+++ lib/dominio/api/api.dart
@@ -4,24 +4,25 @@ import 'package:todo_everis/dominio/model/todo.dart';
 class Api {
   final String _url = "https://5ec6e8bf5961a20016a9eba4.mockapi.io/api/v1/";
   final Dio dio = Dio();
+  Response _response = Response();
 
   Future<List<Todo>> findAll() async {
-    Response response = await dio.get('$_url/task');
-    return (response.data as List).map((todo) => Todo.fromJson(todo)).toList();
+    _response = await dio.get('$_url/task');
+    return (_response.data as List).map((todo) => Todo.fromJson(todo)).toList();
   }
 
   Future<Todo> add(Todo todo) async {
-    Response response = await dio.post('$_url/task', data: todo.toJson());
-    return Todo.fromJson(response.data);
+    _response = await dio.post('$_url/task', data: todo.toJson());
+    return Todo.fromJson(_response.data);
   }
 
   Future<Todo> update(Todo todo) async {
-    Response response = await dio.put('$_url/task/${todo.id}', data: todo.toJson());
-    return Todo.fromJson(response.data);
+    _response = await dio.put('$_url/task/${todo.id}', data: todo.toJson());
+    return Todo.fromJson(_response.data);
   }
 
   Future<dynamic> deleteId(String todoId) async {
-    Response response = await dio.delete('$_url/task/$todoId');
-    return response.data;
+    _response = await dio.delete('$_url/task/$todoId');
+    return _response.data;
   }
 }
